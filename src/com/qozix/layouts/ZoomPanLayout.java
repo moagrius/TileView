5d3d9e72c8219237d26b5373947352aa9eaf128b
Manuel Ohlendorf <moh@icnh.de>
2013-08-09 15:45:46 +0200

    use the scroller-compat from android support

--- src/com/qozix/layouts/ZoomPanLayout.java
+++ src/com/qozix/layouts/ZoomPanLayout.java
@@ -8,6 +8,7 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.widget.ScrollerCompat;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
@@ -17,7 +18,6 @@ import android.view.ViewGroup;
 import com.qozix.animation.Tween;
 import com.qozix.animation.TweenListener;
 import com.qozix.animation.easing.Strong;
-import com.qozix.widgets.Scroller;
 
 /**
  * ZoomPanLayout extends ViewGroup to provide support for scrolling and zooming.  Fling, drag, pinch and
@@ -85,7 +85,7 @@ public class ZoomPanLayout extends ViewGroup {
 	
 	private ScrollActionHandler scrollActionHandler;
 
-	private Scroller scroller;
+	private ScrollerCompat scroller;
 	private VelocityTracker velocity;
 
 	private HashSet<GestureListener> gestureListeners = new HashSet<GestureListener>();
@@ -138,8 +138,8 @@ public class ZoomPanLayout extends ViewGroup {
 		
 		scrollActionHandler = new ScrollActionHandler( this );
 
-		scroller = new Scroller( context );
-		scroller.setFriction( FRICTION );
+		scroller = ScrollerCompat.create( context );
+		//scroller.setFriction( FRICTION );
 
 		clip = new StaticLayout( context );
 		super.addView( clip, -1, new LayoutParams(-1, -1) );
