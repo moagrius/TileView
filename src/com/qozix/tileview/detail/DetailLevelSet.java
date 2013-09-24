04a17d8f4e4f93d07ed08094e553daa8224d3d43
Corey Scott <corey.scott@gmail.com>
2013-09-24 12:25:39 +0800

    added some tile selection options (defaults to previous behaviour)

--- src/com/qozix/tileview/detail/DetailLevelSet.java
+++ src/com/qozix/tileview/detail/DetailLevelSet.java
@@ -12,50 +12,135 @@ import java.util.LinkedList;
  * built in.
  */
 
-public class DetailLevelSet extends LinkedList<DetailLevel> {
+public class DetailLevelSet extends LinkedList<DetailLevel>
+{
 
-	private static final long serialVersionUID = -1742428277010988084L;
+    private static final long serialVersionUID = -1742428277010988084L;
 
-	public void addDetailLevel( DetailLevel detailLevel ) {
-		// ensure uniqueness
-		if ( contains( detailLevel ) ) {
-			return;
-		}
-		// add to the collection
-		add( detailLevel );
-		// sort it
-		Collections.sort( this );
-	}
+    public void addDetailLevel(final DetailLevel detailLevel)
+    {
+        // ensure uniqueness
+        if (this.contains(detailLevel))
+        {
+            return;
+        }
+        // add to the collection
+        this.add(detailLevel);
+        // sort it
+        Collections.sort(this);
+    }
 
-	// get the DetailLevel closest to the passed scale - this could be handled by .floor with a higher API
-	public DetailLevel find( double scale ) {
-		// fast-fail
-		if ( size() == 0 ) {
-			return null;
-		}
-		// set to null initially, but should never fail to populate
-		DetailLevel match = null;
-		// start at the last index
-		int index = size() - 1;
-		// loop from largest to smallest
-		for ( int i = index; i >= 0; i-- ) {
-			// store the iteration level in the return product for now
-			match = get( i );
-			// if the iteration scale is less than the desired scale...
-			if ( match.getScale() < scale ) {
-				// and there's a level registered with a larger scale
-				if ( i < index ) {
-					// ... try to get the next largest
-					match = get( i + 1 );
-					// if we're at the largest level and can't go up one, then we've got our best-case
-				}
-				// we've got a match, all done
-				break;
-			}
-		}
-		return match;
-	}
-	
+    /**
+     * get the DetailLevel next largest scale (this is the default method)
+     * 
+     * (this could be handled by .floor with a higher API)
+     * 
+     * @param scale
+     * @return
+     */
+    public DetailLevel find(final double scale)
+    {
+        // fast-fail
+        if (this.size() == 0)
+        {
+            return null;
+        }
+        // set to null initially, but should never fail to populate
+        DetailLevel match = null;
+        // start at the last index
+        final int index = this.size() - 1;
+        // loop from largest to smallest
+        for (int i = index; i >= 0; i--)
+        {
+            // store the iteration level in the return product for now
+            match = this.get(i);
+            // if the iteration scale is less than the desired scale...
+            if (match.getScale() < scale)
+            {
+                // and there's a level registered with a larger scale
+                if (i < index)
+                {
+                    // ... try to get the next largest
+                    match = this.get(i + 1);
+                    // if we're at the largest level and can't go up one, then
+                    // we've got our best-case
+                }
+                // we've got a match, all done
+                break;
+            }
+        }
+        return match;
+    }
+
+    /**
+     * Find scale that is closest to the requested scale
+     * 
+     * @param scale
+     * @return
+     */
+    public DetailLevel findClosest(final double scale)
+    {
+        // fast-fail
+        if (this.size() == 0)
+        {
+            return null;
+        }
+
+        // default to first item
+        DetailLevel match = null;
+        double diffToMatch = 0d;
+
+        // loop through and find the "closest"
+        for (final DetailLevel thisLevel : this)
+        {
+            if (match == null)
+            {
+                // default to the first value found
+                match = thisLevel;
+                diffToMatch = Math.abs(scale - match.getScale());
+                continue;
+            }
+
+            // calculate the diff from current level to requested scale
+            final double diffToCurrent = Math.abs(scale - thisLevel.getScale());
+            if (diffToCurrent < diffToMatch)
+            {
+                match = thisLevel;
+                diffToMatch = diffToCurrent;
+            }
+        }
+
+        return match;
+    }
+
+    /**
+     * Find by defined scale ranges
+     * 
+     * @see DetailLevel#setRange(min, max)
+     * 
+     * @param scale
+     * @return
+     */
+    public DetailLevel findByDefinedRange(final double scale)
+    {
+
+        // fast-fail
+        if (this.size() == 0)
+        {
+            return null;
+        }
+
+        // loop through and find a set where this scale fits
+        for (final DetailLevel thisLevel : this)
+        {
+            if ((thisLevel.getScaleMin() <= scale) && (thisLevel.getScaleMax() >= scale))
+            {
+                return thisLevel;
+            }
+        }
+
+        // no matches found, no default
+        return null;
+    }
 
 }
-
--- src/com/qozix/tileview/detail/DetailManager.java
+++ src/com/qozix/tileview/detail/DetailManager.java
@@ -4,217 +4,294 @@ import java.util.HashSet;
 
 import android.graphics.Rect;
 
-public class DetailManager {
+public class DetailManager
+{
+    // selection methods for the different tile sets
+    public static final int                         METHOD_DEFAULT            = 0;
+    public static final int                         METHOD_CLOSEST            = 1;
+    public static final int                         METHOD_RANGES             = 2;
 
-	private static final double PRECISION = 6;
-	private static final double DECIMAL = Math.pow( 10, PRECISION );
-	
-	private DetailLevelSet detailLevels = new DetailLevelSet();
-	private HashSet<DetailLevelEventListener> detailLevelEventListeners = new HashSet<DetailLevelEventListener>();
-	private HashSet<DetailLevelSetupListener> detailLevelSetupListeners = new HashSet<DetailLevelSetupListener>();
+    private static final double                     PRECISION                 = 6;
+    private static final double                     DECIMAL                   = Math.pow(10, DetailManager.PRECISION);
 
-	private double scale = 1;
-	private double historicalScale;
-	
-	private DetailLevel currentDetailLevel;
-	
-	private int width;
-	private int height;
-	private int scaledWidth;
-	private int scaledHeight;
-	
-	private boolean detailLevelLocked = false;
-	
-	private int padding = 0;
-	private Rect viewport = new Rect();
-	private Rect computedViewport = new Rect();
-	
-	private DetailLevelPatternParser detailLevelPatternParser = new DetailLevelPatternParserDefault();
+    private final DetailLevelSet                    detailLevels              = new DetailLevelSet();
+    private final HashSet<DetailLevelEventListener> detailLevelEventListeners = new HashSet<DetailLevelEventListener>();
+    private final HashSet<DetailLevelSetupListener> detailLevelSetupListeners = new HashSet<DetailLevelSetupListener>();
 
-	private static double getAtPrecision( double s ) {
-		return Math.round( s * DECIMAL ) / DECIMAL;
-	}
+    private double                                  scale                     = 1;
+    private double                                  historicalScale;
 
-	public DetailManager(){
-		update( true );
-	}
+    private DetailLevel                             currentDetailLevel;
 
-	public double getScale() {
-		return scale;
-	}
+    private int                                     width;
+    private int                                     height;
+    private int                                     scaledWidth;
+    private int                                     scaledHeight;
 
-	public void setScale( double s ) {
-		// round to PRECISION decimal places
-		// DEBUG: why are we rounding still?
-		s = getAtPrecision( s );
-		// is it changed?
-		boolean changed = ( scale != s );
-		// set it
-		scale = s;
-		// update computed values
-		update( changed );		
-	}
-	
-	public int getWidth(){
-		return width;
-	}
-	
-	public int getHeight(){
-		return height;
-	}
-	
-	// DEBUG: needed?  maybe use ZPL's width and height...?
-	public int getScaledWidth(){
-		return scaledWidth;
-	}
-	
-	public int getScaledHeight(){
-		return scaledHeight;
-	}
-	
-	public void setSize( int w, int h ) {
-		width = w;
-		height = h;
-		update( true );
-	}
-	
-	/**
-	 *  "pads" the viewport by the number of pixels passed.  e.g., setPadding( 100 ) instructs the
-	 *  DetailManager to interpret it's actual viewport offset by 100 pixels in each direction (top, left,
-	 *  right, bottom), so more tiles will qualify for "visible" status when intersections are calculated.
-	 * @param pixels (int) the number of pixels to pad the viewport by
-	 */
-	public void setPadding( int pixels ) {
-		padding = pixels;
-		updateComputedViewport();
-	}
-	
-	public void updateViewport( int left, int top, int right, int bottom ) {
-		viewport.set( left, top, right, bottom );
-		updateComputedViewport();
-	}
-	
-	private void updateComputedViewport() {
-		computedViewport.set( viewport );
-		computedViewport.top -= padding;
-		computedViewport.left -= padding;
-		computedViewport.bottom += padding;
-		computedViewport.right += padding;
-	}
-	
-	public Rect getViewport() {
-		return viewport;
-	}
-	
-	public Rect getComputedViewport() {		
-		return computedViewport;
-	}
-	
-	public DetailLevelPatternParser getDetailLevelPatternParser() {
-		return detailLevelPatternParser;
-	}
-	
-	public void setDetailLevelPatternParser( DetailLevelPatternParser parser ) {
-		detailLevelPatternParser = parser;
-	}
-	
-	private void update( boolean changed ){
-		// has there been a change in tile sets?
-		boolean detailLevelChanged = false;		
-		// if detail level is locked, do not change tile sets
-		if(!detailLevelLocked){			
-			// get the most appropriate detail level for the current scale
-			DetailLevel matchingLevel = detailLevels.find( getScale() );
-			// if one is found (if any tile sets are registered)
-			if(matchingLevel != null){
-				// is it the same as the one being used?
-				detailLevelChanged = !matchingLevel.equals( currentDetailLevel );				
-				// update current detail level
-				currentDetailLevel = matchingLevel; 
-			}			
-		}		
-		// update scaled values
-		scaledWidth = (int) ( getWidth() * getScale() );
-		scaledHeight = (int) ( getHeight() * getScale() );
-		// broadcast scale change
-		if( changed ) {
-			for ( DetailLevelEventListener listener : detailLevelEventListeners ) {
-				listener.onDetailScaleChanged( getScale() );
-			}			
-		}
-		// if there's a change in detail, update appropriate values
-		if ( detailLevelChanged ) {			
-			// notify all interested parties
-			for ( DetailLevelEventListener listener : detailLevelEventListeners ) {
-				listener.onDetailLevelChanged();
-			}
-		}
-	}
+    private boolean                                 detailLevelLocked         = false;
 
-	public void lockDetailLevel(){
-		detailLevelLocked = true;
-	}
-	
-	public void unlockDetailLevel(){
-		detailLevelLocked = false;
-	}
+    private int                                     padding                   = 0;
+    private final Rect                              viewport                  = new Rect();
+    private final Rect                              computedViewport          = new Rect();
 
-	public void addDetailLevelEventListener( DetailLevelEventListener l ) {
-		detailLevelEventListeners.add( l );
-	}
+    private DetailLevelPatternParser                detailLevelPatternParser  = new DetailLevelPatternParserDefault();
 
-	public void removeDetailLevelEventListener( DetailLevelEventListener l ) {
-		detailLevelEventListeners.remove( l );
-	}
-	
-	public void addDetailLevelSetupListener( DetailLevelSetupListener l ) {
-		detailLevelSetupListeners.add( l );
-	}
+    // the method used to select between the different tile sets
+    private int                                     selectionMethod           = DetailManager.METHOD_DEFAULT;
 
-	public void removeDetailLevelSetupListener( DetailLevelSetupListener l ) {
-		detailLevelSetupListeners.remove( l );
-	}
-	
-	private void addDetailLevel( DetailLevel detailLevel ) {
-		detailLevels.addDetailLevel( detailLevel );
-		update( false );
-		for ( DetailLevelSetupListener listener : detailLevelSetupListeners ) {
-			listener.onDetailLevelAdded();
-		}
-	}
-	
-	public void addDetailLevel( float scale, String pattern, String downsample ) {
-		DetailLevel detailLevel = new DetailLevel( this, scale, pattern, downsample );
-		addDetailLevel( detailLevel );
-	}
-	
-	public void addDetailLevel( float scale, String pattern, String downsample, int tileWidth, int tileHeight ) {
-		DetailLevel detailLevel = new DetailLevel( this, scale, pattern, downsample, tileWidth, tileHeight );
-		addDetailLevel( detailLevel );
-	}
-	
-	public void resetDetailLevels(){
-		detailLevels.clear();
-		update( false );
-	}
+    private static double getAtPrecision(final double s)
+    {
+        return Math.round(s * DetailManager.DECIMAL) / DetailManager.DECIMAL;
+    }
 
-	public DetailLevel getCurrentDetailLevel() {
-		return currentDetailLevel;
-	}
-	
-	public double getCurrentDetailLevelScale(){
-		if(currentDetailLevel != null ) {
-			return currentDetailLevel.getScale();
-		}
-		return 1;
-	}
-	
-	public double getHistoricalScale(){
-		return historicalScale;
-	}
-	
-	public void saveHistoricalScale(){
-		historicalScale = scale;
-	}
+    public DetailManager()
+    {
+        this.update(true);
+    }
+
+    public double getScale()
+    {
+        return this.scale;
+    }
+
+    public void setScale(double s)
+    {
+        // round to PRECISION decimal places
+        // DEBUG: why are we rounding still?
+        s = DetailManager.getAtPrecision(s);
+        // is it changed?
+        final boolean changed = (this.scale != s);
+        // set it
+        this.scale = s;
+        // update computed values
+        this.update(changed);
+    }
+
+    public int getWidth()
+    {
+        return this.width;
+    }
+
+    public int getHeight()
+    {
+        return this.height;
+    }
+
+    // DEBUG: needed?  maybe use ZPL's width and height...?
+    public int getScaledWidth()
+    {
+        return this.scaledWidth;
+    }
+
+    public int getScaledHeight()
+    {
+        return this.scaledHeight;
+    }
+
+    public void setSize(final int w, final int h)
+    {
+        this.width = w;
+        this.height = h;
+        this.update(true);
+    }
+
+    /**
+     *  "pads" the viewport by the number of pixels passed.  e.g., setPadding( 100 ) instructs the
+     *  DetailManager to interpret it's actual viewport offset by 100 pixels in each direction (top, left,
+     *  right, bottom), so more tiles will qualify for "visible" status when intersections are calculated.
+     * @param pixels (int) the number of pixels to pad the viewport by
+     */
+    public void setPadding(final int pixels)
+    {
+        this.padding = pixels;
+        this.updateComputedViewport();
+    }
+
+    public void updateViewport(final int left, final int top, final int right, final int bottom)
+    {
+        this.viewport.set(left, top, right, bottom);
+        this.updateComputedViewport();
+    }
+
+    private void updateComputedViewport()
+    {
+        this.computedViewport.set(this.viewport);
+        this.computedViewport.top -= this.padding;
+        this.computedViewport.left -= this.padding;
+        this.computedViewport.bottom += this.padding;
+        this.computedViewport.right += this.padding;
+    }
+
+    public Rect getViewport()
+    {
+        return this.viewport;
+    }
+
+    public Rect getComputedViewport()
+    {
+        return this.computedViewport;
+    }
+
+    public DetailLevelPatternParser getDetailLevelPatternParser()
+    {
+        return this.detailLevelPatternParser;
+    }
+
+    public void setDetailLevelPatternParser(final DetailLevelPatternParser parser)
+    {
+        this.detailLevelPatternParser = parser;
+    }
+
+    private void update(final boolean changed)
+    {
+        // has there been a change in tile sets?
+        boolean detailLevelChanged = false;
+        // if detail level is locked, do not change tile sets
+        if (!this.detailLevelLocked)
+        {
+            // get the most appropriate detail level for the current scale
+            // as determined by the selection method
+            DetailLevel matchingLevel = null;
+            switch (this.selectionMethod)
+            {
+                case METHOD_CLOSEST:
+                    matchingLevel = this.detailLevels.findClosest(this.getScale());
+                    break;
+
+                case METHOD_RANGES:
+                    matchingLevel = this.detailLevels.findByDefinedRange(this.getScale());
+                    break;
+
+                case METHOD_DEFAULT:
+                default:
+                    matchingLevel = this.detailLevels.find(this.getScale());
+                    break;
+            }
+            // if one is found (if any tile sets are registered)
+            if (matchingLevel != null)
+            {
+                // is it the same as the one being used?
+                detailLevelChanged = !matchingLevel.equals(this.currentDetailLevel);
+                // update current detail level
+                this.currentDetailLevel = matchingLevel;
+            }
+        }
+        // update scaled values
+        this.scaledWidth = (int) (this.getWidth() * this.getScale());
+        this.scaledHeight = (int) (this.getHeight() * this.getScale());
+        // broadcast scale change
+        if (changed)
+        {
+            for (final DetailLevelEventListener listener : this.detailLevelEventListeners)
+            {
+                listener.onDetailScaleChanged(this.getScale());
+            }
+        }
+        // if there's a change in detail, update appropriate values
+        if (detailLevelChanged)
+        {
+            // notify all interested parties
+            for (final DetailLevelEventListener listener : this.detailLevelEventListeners)
+            {
+                listener.onDetailLevelChanged();
+            }
+        }
+    }
+
+    public void lockDetailLevel()
+    {
+        this.detailLevelLocked = true;
+    }
+
+    public void unlockDetailLevel()
+    {
+        this.detailLevelLocked = false;
+    }
+
+    public void addDetailLevelEventListener(final DetailLevelEventListener l)
+    {
+        this.detailLevelEventListeners.add(l);
+    }
+
+    public void removeDetailLevelEventListener(final DetailLevelEventListener l)
+    {
+        this.detailLevelEventListeners.remove(l);
+    }
+
+    public void addDetailLevelSetupListener(final DetailLevelSetupListener l)
+    {
+        this.detailLevelSetupListeners.add(l);
+    }
+
+    public void removeDetailLevelSetupListener(final DetailLevelSetupListener l)
+    {
+        this.detailLevelSetupListeners.remove(l);
+    }
+
+    private void addDetailLevel(final DetailLevel detailLevel)
+    {
+        this.detailLevels.addDetailLevel(detailLevel);
+        this.update(false);
+        for (final DetailLevelSetupListener listener : this.detailLevelSetupListeners)
+        {
+            listener.onDetailLevelAdded();
+        }
+    }
+
+    public void addDetailLevel(final float scale, final String pattern, final String downsample)
+    {
+        final DetailLevel detailLevel = new DetailLevel(this, scale, pattern, downsample);
+        this.addDetailLevel(detailLevel);
+    }
+
+    public void addDetailLevel(final float scale, final String pattern, final String downsample, final int tileWidth,
+                    final int tileHeight)
+    {
+        final DetailLevel detailLevel = new DetailLevel(this, scale, pattern, downsample, tileWidth, tileHeight);
+        this.addDetailLevel(detailLevel);
+    }
+
+    public void addDetailLevel(final float scale, final String pattern, final String downsample, final int tileWidth,
+                    final int tileHeight, final double scaleMin, final double scaleMax)
+    {
+        final DetailLevel detailLevel = new DetailLevel(this, scale, pattern, downsample, tileWidth, tileHeight,
+                        scaleMin, scaleMax);
+        this.addDetailLevel(detailLevel);
+    }
+
+    public void resetDetailLevels()
+    {
+        this.detailLevels.clear();
+        this.update(false);
+    }
+
+    public DetailLevel getCurrentDetailLevel()
+    {
+        return this.currentDetailLevel;
+    }
+
+    public double getCurrentDetailLevelScale()
+    {
+        if (this.currentDetailLevel != null)
+        {
+            return this.currentDetailLevel.getScale();
+        }
+        return 1;
+    }
+
+    public double getHistoricalScale()
+    {
+        return this.historicalScale;
+    }
+
+    public void saveHistoricalScale()
+    {
+        this.historicalScale = this.scale;
+    }
+
+    public void setTileSelectionMethod(final int method)
+    {
+        this.selectionMethod = method;
+    }
 
 }
