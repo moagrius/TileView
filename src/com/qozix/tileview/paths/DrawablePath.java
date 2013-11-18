79b4201da4bea2ff50daf06cc4975f25254e7f06
Corey Scott <corey.scott@gmail.com>
2013-11-18 14:02:21 +0800

    Added support for custom path drawing

--- src/com/qozix/tileview/paths/DrawablePath.java
+++ src/com/qozix/tileview/paths/DrawablePath.java
@@ -1,11 +1,45 @@
 package com.qozix.tileview.paths;
 
+import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 
-public class DrawablePath {
+public interface DrawablePath {
 
-	public Path path;
-	public Paint paint;
+    /**
+     * Returns the path that this drawable will follow.
+     * 
+     * @return
+     */
+    public Path getPath();
+
+    /**
+     * Set path for this drawable.
+     * 
+     * @param path
+     */
+    public void setPath(final Path path);
+
+    /**
+     * Returns the paint to be used for this path.
+     * 
+     * @return
+     */
+    public Paint getPaint();
+
+    /**
+     * Set the paint for this path.
+     * 
+     * @param paint
+     */
+    public void setPaint(final Paint paint);
+
+    /**
+     * Draw the supplied path onto the supplied canvas.
+     * 
+     * @param canvas
+     * @param drawingPath
+     */
+    public void draw(final Canvas canvas, final Path drawingPath);
 
 }
--- src/com/qozix/tileview/paths/PathManager.java
+++ src/com/qozix/tileview/paths/PathManager.java
@@ -13,116 +13,128 @@ import android.graphics.Point;
 
 import com.qozix.layouts.StaticLayout;
 import com.qozix.tileview.detail.DetailManager;
+import com.qozix.tileview.paths.drawables.SolidDrawablePath;
 
 /**
- * Canvas.drawPath takes a lot of resources.  Using Canvas.drawLines is more performant,
- * but looks terrible for anything beyond a 1px plain line.  Creating and managing a bitmap
- * either scales the thickness of the path, or if you manage that, ends up taking as much
- * juice as Canvas.drawPath.  After many iterations, the default implementation just offers
- * path drawing via Canvas.drawPath.  If you are using very large paths, or need more customization,
- * consider a custom implementation (most likely a custom View subclass added as child view of the
- * TileView, with an overriden onDraw method that manages paths in whatever fashion is most
- * appropriate to your need, or possibly a SurfaceView, or even OpenGL.
- * ref (http://stackoverflow.com/a/15208783/429430)
+ * Canvas.drawPath takes a lot of resources. Using Canvas.drawLines is more
+ * performant, but looks terrible for anything beyond a 1px plain line. Creating
+ * and managing a bitmap either scales the thickness of the path, or if you
+ * manage that, ends up taking as much juice as Canvas.drawPath. After many
+ * iterations, the default implementation just offers path drawing via
+ * Canvas.drawPath. If you are using very large paths, or need more
+ * customization, consider a custom implementation (most likely a custom View
+ * subclass added as child view of the TileView, with an overriden onDraw method
+ * that manages paths in whatever fashion is most appropriate to your need, or
+ * possibly a SurfaceView, or even OpenGL. ref
+ * (http://stackoverflow.com/a/15208783/429430)
  */
 public class PathManager extends StaticLayout {
 
-	private static final int DEFAULT_STROKE_COLOR = 0x883399FF;
-	private static final int DEFAULT_STROKE_WIDTH = 8;
+    private static final int DEFAULT_STROKE_COLOR = 0x883399FF;
+    private static final int DEFAULT_STROKE_WIDTH = 8;
 
-	private boolean shouldDraw = true;
+    private boolean shouldDraw = true;
 
-	private Paint defaultPaint = new Paint();
-	{
-		defaultPaint.setStyle( Paint.Style.STROKE );
-		defaultPaint.setColor( DEFAULT_STROKE_COLOR );
-		defaultPaint.setStrokeWidth( DEFAULT_STROKE_WIDTH );
-		defaultPaint.setAntiAlias( true );
-	}
+    private Paint defaultPaint = new Paint();
+    {
+        defaultPaint.setStyle(Paint.Style.STROKE);
+        defaultPaint.setColor(DEFAULT_STROKE_COLOR);
+        defaultPaint.setStrokeWidth(DEFAULT_STROKE_WIDTH);
+        defaultPaint.setAntiAlias(true);
+    }
 
-	private DetailManager detailManager;
+    private DetailManager detailManager;
 
-	private Path drawingPath = new Path();
-	private Matrix matrix = new Matrix();
+    private Path drawingPath = new Path();
+    private Matrix matrix = new Matrix();
 
-	private ArrayList<DrawablePath> paths = new ArrayList<DrawablePath>();
+    private ArrayList<DrawablePath> paths = new ArrayList<DrawablePath>();
 
-	public PathManager( Context context, DetailManager dm ) {
-		super( context );
-		setWillNotDraw( false );
-		detailManager = dm;
-	}
+    public PathManager(Context context, DetailManager dm) {
+        super(context);
+        setWillNotDraw(false);
+        detailManager = dm;
+    }
 
-	public Paint getPaint() {
-		return defaultPaint;
-	}
+    public Paint getPaint() {
+        return defaultPaint;
+    }
 
-	public DrawablePath addPath( List<Point> points ) {
-		return addPath( points, defaultPaint );
-	}
+    public Path getPathFromPoints(List<Point> points) {
+        Path path = new Path();
+        Point start = points.get(0);
+        path.moveTo((float) start.x, (float) start.y);
+        int l = points.size();
+        for (int i = 1; i < l; i++) {
+            Point point = points.get(i);
+            path.lineTo((float) point.x, (float) point.y);
+        }
+        return path;
+    }
 
-	public DrawablePath addPath( List<Point> points, Paint paint ) {
-		Path path = new Path();
-		Point start = points.get( 0 );
-		path.moveTo( (float) start.x, (float) start.y );
-		int l = points.size();
-		for ( int i = 1; i < l; i++ ) {
-			Point point = points.get( i );
-			path.lineTo( (float) point.x, (float) point.y );
-		}
-		return addPath( path, paint );
-	}
+    public DrawablePath addPath(List<Point> points) {
+        return addPath(points, defaultPaint);
+    }
 
-	public DrawablePath addPath( Path path, Paint paint ) {
-		DrawablePath drawablePath = new DrawablePath();
-		drawablePath.path = path;
-		drawablePath.paint = paint;
-		return addPath( drawablePath );
-	}
+    public DrawablePath addPath(List<Point> points, Paint paint) {
+        Path path = new Path();
+        Point start = points.get(0);
+        path.moveTo((float) start.x, (float) start.y);
+        int l = points.size();
+        for (int i = 1; i < l; i++) {
+            Point point = points.get(i);
+            path.lineTo((float) point.x, (float) point.y);
+        }
+        return addPath(path, paint);
+    }
 
-	public DrawablePath addPath( Path path ) {
-		return addPath( path, defaultPaint );
-	}
+    public DrawablePath addPath(Path path, Paint paint) {
+        DrawablePath drawablePath = new SolidDrawablePath();
+        drawablePath.setPath(path);
+        drawablePath.setPaint(paint);
+        return addPath(drawablePath);
+    }
 
-	public DrawablePath addPath( DrawablePath drawablePath ) {
-		paths.add( drawablePath );
-		invalidate();
-		return drawablePath;
-	}
+    public DrawablePath addPath(Path path) {
+        return addPath(path, defaultPaint);
+    }
 
-	public void removePath( DrawablePath path ) {
-		paths.remove( path );
-		invalidate();
-	}
+    public DrawablePath addPath(DrawablePath drawablePath) {
+        paths.add(drawablePath);
+        invalidate();
+        return drawablePath;
+    }
 
-	public void clear() {
-		paths.clear();
-		invalidate();
-	}
+    public void removePath(DrawablePath path) {
+        paths.remove(path);
+        invalidate();
+    }
 
-	public void setShouldDraw( boolean should ) {
-		shouldDraw = should;
-		invalidate();
-	}
+    public void clear() {
+        paths.clear();
+        invalidate();
+    }
 
-	@SuppressLint("NewApi")
-	@Override
-	public void onDraw( Canvas canvas ) {
-		if ( shouldDraw ) {
-			float scale = (float) detailManager.getScale();
-			matrix.setScale( scale, scale );
-			for ( DrawablePath drawablePath : paths ) {
-				drawingPath.set( drawablePath.path );
-				drawingPath.transform( matrix );
-				// quickReject is not supported on hw accelerated canvas versions below 16 but isHardwareAccelerated works only from version 11
-				if (android.os.Build.VERSION.SDK_INT >= 11 && canvas.isHardwareAccelerated() && android.os.Build.VERSION.SDK_INT < 16) {
-					canvas.drawPath( drawingPath, drawablePath.paint );
-				} else if ( !canvas.quickReject( drawingPath, Canvas.EdgeType.BW ) ) {
-					canvas.drawPath( drawingPath, drawablePath.paint );
-				}
-			}
-		}
-		super.onDraw( canvas );
-	}
+    public void setShouldDraw(boolean should) {
+        shouldDraw = should;
+        invalidate();
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onDraw(Canvas canvas) {
+        if (shouldDraw) {
+            float scale = (float) detailManager.getScale();
+            matrix.setScale(scale, scale);
+            for (DrawablePath drawablePath : paths) {
+                drawingPath.set(drawablePath.getPath());
+                drawingPath.transform(matrix);
+
+                // defer drawing to the path object
+                drawablePath.draw(canvas, drawingPath);
+            }
+        }
+        super.onDraw(canvas);
+    }
 
 }
