14c54781638c823d2e3aca7762adaa8a64a394f5
Mike Dunn <mdunn@thedailydot.com>
2016-03-14 11:27:53 -0500

    updating readme, demos

--- demo/src/main/java/tileview/demo/RealMapInternetTileViewActivity.java
+++ demo/src/main/java/tileview/demo/RealMapInternetTileViewActivity.java
@@ -32,18 +32,11 @@ public class RealMapInternetTileViewActivity extends TileViewActivity {
     tileView.addDetailLevel( 0.5000f, "https://raw.githubusercontent.com/moagrius/TileView/master/demo/src/main/assets/tiles/map/phi-250000-%d_%d.jpg" );
     tileView.addDetailLevel( 1.0000f, "https://raw.githubusercontent.com/moagrius/TileView/master/demo/src/main/assets/tiles/map/phi-500000-%d_%d.jpg" );
 
-
-    // set mScale to 0, but keep scaleToFit true, so it'll be as small as possible but still match the container
-    //tileView.setScale( 0 );
-
     // let's use 0-1 positioning...
     tileView.defineBounds( 0, 0, 1, 1 );
 
     // frame to center
-    //frameTo( 0.5, 0.5 );
-
-    // render while panning
-    tileView.setShouldRenderWhilePanning( true );
+    frameTo( 0.5, 0.5 );
 
   }
 }
