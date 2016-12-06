60500bd34f67ea5c2f8ad1c0f9757722c77db2ca
Alan Rager <alan.rager@plangrid.com>
2016-12-05 23:40:01 -0800

    stop being cheeky

--- tileview/src/main/java/com/qozix/tileview/graphics/BitmapRecyclerDefault.java
+++ tileview/src/main/java/com/qozix/tileview/graphics/BitmapRecyclerDefault.java
@@ -4,9 +4,9 @@ import android.graphics.Bitmap;
 
 public class BitmapRecyclerDefault implements BitmapRecycler {
   @Override
-  public void recycleBitmap(Bitmap soylentGreen ) {
-    if( ! soylentGreen.isRecycled() ) {
-      soylentGreen.recycle();
+  public void recycleBitmap(Bitmap bitmap) {
+    if( ! bitmap.isRecycled() ) {
+      bitmap.recycle();
     }
   }
 }
