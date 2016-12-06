85810e138067d1220bbe67211d58363cb5482bf0
Alan Rager <alan.rager@plangrid.com>
2016-12-05 23:40:47 -0800

    remove unneccessary space

--- tileview/src/main/java/com/qozix/tileview/graphics/BitmapRecyclerDefault.java
+++ tileview/src/main/java/com/qozix/tileview/graphics/BitmapRecyclerDefault.java
@@ -5,7 +5,7 @@ import android.graphics.Bitmap;
 public class BitmapRecyclerDefault implements BitmapRecycler {
   @Override
   public void recycleBitmap(Bitmap bitmap) {
-    if( ! bitmap.isRecycled() ) {
+    if( !bitmap.isRecycled() ) {
       bitmap.recycle();
     }
   }
