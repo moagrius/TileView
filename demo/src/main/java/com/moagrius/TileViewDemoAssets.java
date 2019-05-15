f579dbca23c3502c10f4a934414531cc0f1d480d
Mike Dunn <mdunn@safaribooksonline.com>
2019-05-15 09:34:24 -0500

    limit zoom to minimum of 0 when computing

--- demo/src/main/java/com/moagrius/TileViewDemoAssets.java
+++ demo/src/main/java/com/moagrius/TileViewDemoAssets.java
@@ -12,10 +12,10 @@ public class TileViewDemoAssets extends TileViewDemoActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_demos_tileview);
     TileView tileView = findViewById(R.id.tileview);
+    tileView.setScaleLimits(1f, 2f);
     new TileView.Builder(tileView)
         .setSize(17934, 13452)
         .defineZoomLevel("tiles/phi-1000000-%1$d_%2$d.jpg")
-        .addReadyListener(this)
         .build();
 
   }
